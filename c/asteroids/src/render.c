0976fd9350f5d4ad4bc6e8a03b0ae7f2bcf685c9
Anton Gushcha <ncrashed@gmail.com>
2020-06-20 17:55:24 +0000

    Render asteroids

--- c/asteroids/src/render.c
+++ c/asteroids/src/render.c
@@ -10,6 +10,24 @@ void render_line(SDL_Renderer *renderer, struct v2f pos, float rotation, struct 
                              , pos.x + p2.x, pos.y + p2.y);
 }
 
+struct v2f circloid_point(int i, int n, float r) {
+  float a = (float)i * (2 * M_PI / (float)n);
+  return (struct v2f) {
+      .x = r * (1 + sin(a) * 0.3) * cos(a)
+    , .y = r * (1 + cos(a) * 0.3) * sin(a)
+    };
+}
+
+void render_circloid(SDL_Renderer *renderer, int n, float a0, struct v2f pos, float r) {
+  for(int i=0; i <= n-1; i++) {
+    render_line(renderer, pos, a0, circloid_point(i, n, r), circloid_point(i+1, n, r));
+  }
+}
+
+void render_asteroid(SDL_Renderer *renderer, struct asteroid_component asteroid, struct v2f pos, float rotation) {
+  render_circloid(renderer, asteroid.edges, rotation, pos, asteroid.radius);
+}
+
 void render_player(SDL_Renderer *renderer, struct player_component player, struct v2f pos, float rotation) {
   render_line(renderer, pos, rotation, (struct v2f) { .x =  PLAYER_DX, .y =  0 }
                                      , (struct v2f) { .x = -PLAYER_DX, .y = -PLAYER_DY});
@@ -27,6 +45,13 @@ void render_player(SDL_Renderer *renderer, struct player_component player, struc
 
 void render_entity(SDL_Renderer *renderer, entity e, const struct World *world) {
   if (entity_has_component(e, COMPONENT_POSITION | COMPONENT_ROTATION | COMPONENT_PLAYER, world->tags)) {
-     render_player(renderer, world->player.unique, world->position[e], world->rotation[e]);
+    render_player(renderer, world->player.unique
+      , *get_position_component_const(e, &world->position)
+      , *get_rotation_component_const(e, &world->rotation) );
+  } else if (entity_has_component(e, COMPONENT_POSITION | COMPONENT_ROTATION | COMPONENT_ASTEROID, world->tags)) {
+    render_asteroid(renderer
+      , *get_asteroid_component_const(e, &world->asteroid)
+      , *get_position_component_const(e, &world->position)
+      , *get_rotation_component_const(e, &world->rotation) );
   }
 }
