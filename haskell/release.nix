5e09f382a203ae13db2e9f35656b3ae1b7138899
Anton Gushcha <ncrashed@gmail.com>
2020-07-23 23:24:12 +0300

    Use ghc-8.10.1

--- haskell/release.nix
+++ haskell/release.nix
@@ -3,12 +3,13 @@ let
   project = import ((nixpkgs {}).fetchFromGitHub {
     owner = "NCrashed";
     repo = "haskell-nix";
-    rev = "b1fcade2695a48ef7cff2833b9d0c5662de70dbb";
-    sha256  = "1vnbi1g6yswq5vb1fqjmh2mdnzynka2v17rnyh9j170xnza3crg2";
+    rev = "0c1c27a22daa78d359d7704448e4c6e2512cde5d";
+    sha256  = "1vnbi1g6ydwq5vb1fqjmh2mdnzynka2v17rnyh9j170xnza3crg2";
   }) { inherit nixpkgs; };
 in project {
   packages = {
     asteroids = ./asteroids;
     plotting = ./plotting;
   };
+  compiler = "ghc8101";
 }
