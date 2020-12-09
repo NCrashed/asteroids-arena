5cd1eee66c4c5d00da805d881b7f2ff8dd5eacda
Anton Gushcha <ncrashed@protonmail.com>
2020-12-09 15:40:27 +0300

    Add rust stub

---
+++ rust/shell.nix
@@ -0,0 +1,13 @@
+with import ../nix/pkgs.nix {};
+
+stdenv.mkDerivation rec {
+  name = "asteroids-rust-env";
+  env = buildEnv { name = name; paths = buildInputs; };
+
+  buildInputs = [
+    rustup
+    rust-analyzer
+    SDL2
+    SDL2_mixer
+  ];
+}
