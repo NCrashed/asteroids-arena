ced406c9c160b91f951f5ce24a1e7221cdbdd702
Anton Gushcha <ncrashed@protonmail.com>
2020-12-15 01:38:02 +0300

    Add zig nix expression

---
+++ zig/shell.nix
@@ -0,0 +1,18 @@
+with import ../nix/pkgs.nix {
+    config = {
+      packageOverrides = pkgs: {
+        zig = pkgs.callPackage ../nix/zig.nix {};
+      };
+    };
+  };
+
+stdenv.mkDerivation rec {
+  name = "asteroids-zig-env";
+  env = buildEnv { name = name; paths = buildInputs; };
+
+  buildInputs = [
+    zig
+    SDL2
+    SDL2_mixer
+  ];
+}
